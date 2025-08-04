75924df8499e5c2932fa46d7d658435449af241c
Seiji Sato <seigifabian@gmail.com>
2025-08-04 13:08:36 +1000

    Finished the implmementation of the ToDoList

---
+++ src/ToDoItem.java
@@ -0,0 +1,32 @@
+public class ToDoItem {
+
+    private String description;
+    private boolean isDone;
+
+    // TODO Now:
+    // Add a constructor to initialize the item with the description, and isDone as false, with a single parameter for the description
+    // Add getters and setters for each field
+    public ToDoItem(String description){
+        this.description = description;
+        isDone = false;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setIsDone(boolean isDone){
+        this.isDone = isDone;
+    }
+
+    public boolean getIsDone(){
+        return isDone;
+    }
+
+
+
+}
